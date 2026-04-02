b16b623f303315b69fd81698570101f4d93cd731
Brainy7980 <bpurdy12@icloud.com>
2026-04-02 14:25:23 -0700

    Update VideoSprite.hx

--- source/objects/VideoSprite.hx
+++ source/objects/VideoSprite.hx
@@ -1,4 +1,8 @@
+package objects;
+
+#if hxvlc
 import hxcodec.vlc.LibVLC;
 import hxcodec.flixel.FlxVideoSprite;
 
-//todo (also going to replace once HXVLC gets in my hands)
+class VideoSprite extends FlxSpriteGroup {}
+#end
